11cb5eb5caacda07d0bb7b1ff45f001395b0db4b
Kim Fake <58000359+krfake@users.noreply.github.com>
2022-03-08 09:13:46 -0600

    add files

---
+++ visualizing data with ggplot2 examples.R
@@ -0,0 +1,14 @@
+##################################################################
+# The purpose of this R script is to demonstrate data visualization 
+# with ggplot. This was made mostly for my own reference and a way to 
+# practice these functions so it is far from perfect or comprehensive,  
+# but I will share it for others who may find it helpful.
+#
+# Script by: Kim Fake
+#
+#
+###################################################################
+
+# To Clear working environment
+rm(list=ls())
+graphics.off()
